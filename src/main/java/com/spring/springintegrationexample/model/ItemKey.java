4e6eef5cdfa3cad1564b7690a4ffd152b2533c9a
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-11-20 13:50:07 +0530

    include hashing for ItemKey

--- src/main/java/com/spring/springintegrationexample/model/ItemKey.java
+++ src/main/java/com/spring/springintegrationexample/model/ItemKey.java
@@ -1,9 +1,13 @@
 package com.spring.springintegrationexample.model;
 
-public class ItemKey {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ItemKey implements Serializable {
 
     private String formattedDate;
     private String category;
+    private int hash;
 
     public String getCategory() {
         return category;
@@ -21,11 +25,36 @@ public class ItemKey {
         this.formattedDate = formattedDate;
     }
 
+    public int getHash() {
+        if (this.hash == 0)
+            this.hash = this.hashCode();
+        return hash;
+    }
+
+    public void setHash(int hash) {
+        this.hash = hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemKey itemKey = (ItemKey) o;
+        return Objects.equals(formattedDate, itemKey.formattedDate) &&
+                Objects.equals(category, itemKey.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(formattedDate, category);
+    }
+
     @Override
     public String toString() {
         return "ItemKey{" +
                 "formattedDate='" + formattedDate + '\'' +
                 ", category='" + category + '\'' +
+                ", hash=" + getHash() +
                 '}';
     }
 }
